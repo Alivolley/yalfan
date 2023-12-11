495649c27ea8593c0a8cefbdeceda7b3444cace9
alivolley <alicryptovolley@gmail.com>
2023-12-11 13:04:10 +0330

    home : off banners pictures added

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,14 @@
 // Components
 import Banner from '@/components/pages/home/banner/banner';
 import Categories from '@/components/pages/home/categories/categories';
+import OffersBanner from '@/components/pages/home/offers-banner/offers-banner';
 
 export default function Home() {
    return (
       <main>
          <Banner />
          <Categories />
+         <OffersBanner />
       </main>
    );
 }
