fc6bc2ab499b8a4024e3f8808e9488a93f441576
alivolley <alicryptovolley@gmail.com>
2024-01-22 17:33:57 +0330

    app js changed

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:20.11-alpine
+
+WORKDIR /frontend
+
+COPY package*.json ./
+
+RUN npm install -g npm@latest
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+CMD ["npm", "start"]
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { NextIntlClientProvider } from 'next-intl';
 import { useRouter } from 'next/router';
 import AppLayout from '@/components/layout/app-layout/app-layout';
@@ -6,16 +5,10 @@ import '@/styles/globals.css';
 import '@/styles/reset.css';
 
 export default function App({ Component, pageProps }) {
-   const { locale } = useRouter();
+   const router = useRouter();
 
    return (
-      <NextIntlClientProvider locale={locale} messages={pageProps.messages}>
-         <Head>
-            <title>{locale === 'fa' ? 'یلفان' : 'Yalfan'}</title>
-            <meta name="description" content="Your website description here" />
-            <meta name="keywords" content="comma, separated, keywords" />
-            <meta name="author" content={locale === 'fa' ? 'یلفان' : 'Yalfan'} />
-         </Head>
+      <NextIntlClientProvider locale={router.locale} messages={pageProps.messages}>
          <AppLayout>
             <Component {...pageProps} />
          </AppLayout>
