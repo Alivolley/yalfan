11868f2f7882f4586e602f273c530378924054a4
alivolley <alicryptovolley@gmail.com>
2024-01-22 18:37:54 +0330

    docker changed

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { NextIntlClientProvider } from 'next-intl';
 import { useRouter } from 'next/router';
 import AppLayout from '@/components/layout/app-layout/app-layout';
@@ -5,10 +6,16 @@ import '@/styles/globals.css';
 import '@/styles/reset.css';
 
 export default function App({ Component, pageProps }) {
-   const router = useRouter();
+   const { locale } = useRouter();
 
    return (
-      <NextIntlClientProvider locale={router.locale} messages={pageProps.messages}>
+      <NextIntlClientProvider locale={locale} messages={pageProps.messages}>
+         <Head>
+            <title>{locale === 'fa' ? 'یلفان' : 'Yalfan'}</title>
+            <meta name="description" content="Your website description here" />
+            <meta name="keywords" content="comma, separated, keywords" />
+            <meta name="author" content={locale === 'fa' ? 'یلفان' : 'Yalfan'} />
+         </Head>
          <AppLayout>
             <Component {...pageProps} />
          </AppLayout>
