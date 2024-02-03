8e85eec97b2277b69689815c75d1472f153c4a77
alivolley <alicryptovolley@gmail.com>
2024-02-03 18:40:05 +0330

    url printed in login and catch

--- apis/login/useVerificationCode.js
+++ apis/login/useVerificationCode.js
@@ -4,12 +4,19 @@ import axiosInstance from '@/configs/axiosInstance';
 
 const useVerificationCode = () =>
    useSWRMutation('accounts/send-verification-code/', (url, data) =>
-      axiosInstance.post(url, data.arg).then(res => {
-         console.log('====================================================');
-         console.log(res?.request?.responseURL);
-         console.log('====================================================');
-         return res.data;
-      })
+      axiosInstance
+         .post(url, data.arg)
+         .then(res => {
+            console.log('====================================================');
+            console.log(res?.request?.responseURL);
+            console.log('====================================================');
+            return res.data;
+         })
+         .catch(err => {
+            console.log('====================================================');
+            console.log(err?.request?.responseURL);
+            console.log('====================================================');
+         })
    );
 
 export default useVerificationCode;
