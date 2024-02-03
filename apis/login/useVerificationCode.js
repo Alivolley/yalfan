032a97e6fa346659e72ebcc2435e1306ba527395
alivolley <alicryptovolley@gmail.com>
2024-02-03 18:30:31 +0330

    url printed in login

--- apis/login/useVerificationCode.js
+++ apis/login/useVerificationCode.js
@@ -4,7 +4,12 @@ import axiosInstance from '@/configs/axiosInstance';
 
 const useVerificationCode = () =>
    useSWRMutation('accounts/send-verification-code/', (url, data) =>
-      axiosInstance.post(url, data.arg).then(res => res.data)
+      axiosInstance.post(url, data.arg).then(res => {
+         console.log('====================================================');
+         console.log(res?.request?.responseURL);
+         console.log('====================================================');
+         return res.data;
+      })
    );
 
 export default useVerificationCode;
