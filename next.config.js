59f55ec43c3598c85af0bfda1b17eddaff0de882
alivolley <alicryptovolley@gmail.com>
2024-02-08 19:17:29 +0330

    axios config base url changed

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 
+const mainBaseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const sliced = mainBaseURL?.slice(8);
+const baseURl = sliced?.slice(0, -1);
+
 const withTM = require('next-transpile-modules')(['mui-one-time-password-input']);
 
 const nextConfig = {
@@ -8,7 +12,7 @@ const nextConfig = {
    images: {
       remotePatterns: [
          {
-            hostname: 'yalfantest.pythonanywhere.com',
+            hostname: baseURl,
          },
       ],
    },
